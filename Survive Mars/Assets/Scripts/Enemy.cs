7b7726e46042cbce0d93b72702e8af8f92b8b60c
Adam Lynch <alynch14@students.towson.edu>
2019-01-20 00:50:37 -0500

    Created enemy prefab

---
+++ Survive Mars/Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject player;
+    public float acceleration;
+    private Vector3 newDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
